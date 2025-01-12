901bfc01c58b64ddd7a0da88f7a5379426c0ba00
Malave615 <malavetracy@gmail.com>
2025-01-12 14:20:40 -0500

    Resolving merge conflicts

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,70 @@
-import { StyleSheet, View, Text } from 'react-native';
-import React, { useEffect } from 'react';
+import { Platform, StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
   const { name, backgroundColor } = route.params;
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
-  }, []);
+    // Set the initial message
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer!',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, [name, navigation]);
+
+  // Function to handle sending new messages
+  const onSend = (newMessages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages),
+    );
+  };
+
+  // Function to customize chat bubble color
+  const renderBubble = (props) => (
+    <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: '#000', // Sender's bubble
+        },
+        left: {
+          backgroundColor: '#FFF', // Receiver's bubble
+        },
+      }}
+    />
+  );
 
   return (
     <View style={[styles.container, { backgroundColor }]}>
-      <Text style={styles.text}>Hello, {name}!</Text>
+      {/* GiftedChat component to display chat messages */}
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === 'android' && <KeyboardAvoidingView behavior="height" />}
+      {Platform.OS === 'ios' && <KeyboardAvoidingView behavior="padding" />}
     </View>
   );
 };
@@ -31,13 +84,6 @@ Chat.propTypes = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 16,
-    fontWeight: '300',
-    color: '#757083',
   },
 });
 
--- components/Start.js
+++ components/Start.js
@@ -7,6 +7,8 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
 } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
 
@@ -19,76 +21,89 @@ const Start = ({ navigation }) => {
     navigation.setOptions({
       title: 'Welcome',
       headerTitleStyle: styles.headerTitle,
+      headerStyle: { backgroundColor: '#090C08' },
     });
   }, [navigation]);
 
   // Navigate to the Chat screen, passing name and color parameters
-  const handleStartChat = () => {
-    navigation.navigate('Chat', { name, backgroundColor });
-  };
+  // const handleStartChat = () => {
+  //   navigation.navigate('Chat', { name, backgroundColor });
+  // };
 
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require('../assets/background.png')}
-        style={styles.background}
-      >
-        <View style={styles.contentContainer}>
-          <View style={styles.inputContainer}>
-            <View style={styles.textInputWrapper}>
-              {/* Name input */}
-              <TextInput
-                style={styles.textInput}
-                value={name}
-                onChangeText={setName}
-                placeholder="Your name"
-                placeholderTextColor="#757083"
-              />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <View style={styles.container}>
+        {/* Background Image */}
+        <ImageBackground
+          source={require('../assets/background.png')}
+          style={styles.background}
+        >
+          <View style={styles.contentContainer}>
+            <Text style={styles.titleText}>Let's Chat</Text>
 
-              {/* SVG Icon as a placeholder */}
-              <Svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 20 19"
-                style={styles.iconImage}
+            {/* Input and Options Container */}
+            <View style={styles.inputContainer}>
+              <View style={styles.textInputWrapper}>
+                {/* Name input */}
+                <TextInput
+                  style={styles.textInput}
+                  value={name}
+                  onChangeText={setName}
+                  placeholder="Your name"
+                  placeholderTextColor="#757083"
+                />
+
+                {/* SVG Icon as a placeholder */}
+                <Svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 19"
+                  style={styles.iconImage}
+                >
+                  <Path
+                    d="M12,13.2533333 C15.24,13.2533333 21.6,14.830125 21.6,18.105 L21.6,20.5308333 L2.4,20.5308333 L2.4,18.105 C2.4,14.830125 8.76,13.2533333 12,13.2533333 Z M20.64,19.5708333 L20.64,18.105 C20.64,16.0913979 15.9773097,14.2133333 12,14.2133333 C8.02269035,14.2133333 3.36,16.0913979 3.36,18.105 L3.36,19.5708333 L20.64,19.5708333 Z M12,11.36 C9.624,11.36 7.68,9.443 7.68,7.1 C7.68,4.757 9.624,2.84 12,2.84 C14.376,2.84 16.32,4.757 16.32,7.1 C16.32,9.443 14.376,11.36 12,11.36 Z M12,10.4 C13.8487889,10.4 15.36,8.90977792 15.36,7.1 C15.36,5.29022208 13.8487889,3.8 12,3.8 C10.1512111,3.8 8.64,5.29022208 8.64,7.1 C8.64,8.90977792 10.1512111,10.4 12,10.4 Z"
+                    fill="#757083"
+                  />
+                </Svg>
+              </View>
+
+              {/* Color selection */}
+              <Text style={styles.colorText}>Choose Background Color:</Text>
+              <View style={styles.colorContainer}>
+                {colors.map((color) => (
+                  <TouchableOpacity
+                    key={color}
+                    style={[
+                      styles.colorOption,
+                      { backgroundColor: color },
+                      backgroundColor === color && styles.selectedColor,
+                    ]}
+                    onPress={() => setBackgroundColor(color)}
+                  />
+                ))}
+              </View>
+
+              {/* Start chat button */}
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() =>
+                  navigation.navigate('Chat', {
+                    name: name || 'User',
+                    backgroundColor,
+                  })
+                }
               >
-                <Path
-                  d="M12,13.2533333 C15.24,13.2533333 21.6,14.830125 21.6,18.105 L21.6,20.5308333 L2.4,20.5308333 L2.4,18.105 C2.4,14.830125 8.76,13.2533333 12,13.2533333 Z M20.64,19.5708333 L20.64,18.105 C20.64,16.0913979 15.9773097,14.2133333 12,14.2133333 C8.02269035,14.2133333 3.36,16.0913979 3.36,18.105 L3.36,19.5708333 L20.64,19.5708333 Z M12,11.36 C9.624,11.36 7.68,9.443 7.68,7.1 C7.68,4.757 9.624,2.84 12,2.84 C14.376,2.84 16.32,4.757 16.32,7.1 C16.32,9.443 14.376,11.36 12,11.36 Z M12,10.4 C13.8487889,10.4 15.36,8.90977792 15.36,7.1 C15.36,5.29022208 13.8487889,3.8 12,3.8 C10.1512111,3.8 8.64,5.29022208 8.64,7.1 C8.64,8.90977792 10.1512111,10.4 12,10.4 Z"
-                  fill="#757083"
-                />
-              </Svg>
+                <Text style={styles.buttonText}>Start Chatting</Text>
+              </TouchableOpacity>
             </View>
-
-            {/* Color selection */}
-            <Text style={styles.colorText}>Choose Background Color:</Text>
-            <View style={styles.colorContainer}>
-              {colors.map((color) => (
-                <TouchableOpacity
-                  key={color}
-                  style={[
-                    styles.colorOption,
-                    { backgroundColor: color },
-                    backgroundColor === color && styles.selectedColor,
-                  ]}
-                  onPress={() => setBackgroundColor(color)}
-                />
-              ))}
-            </View>
-
-            {/* Start chat button */}
-            <TouchableOpacity
-              accessible={true}
-              accessibilityLabel="Start Chatting"
-              style={styles.button}
-              onPress={handleStartChat}
-            >
-              <Text style={styles.buttonText}>Start Chatting</Text>
-            </TouchableOpacity>
           </View>
-        </View>
-      </ImageBackground>
-    </View>
+        </ImageBackground>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -113,10 +128,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingBottom: '30%',
   },
-  headerTitle: {
+  titleText: {
     fontSize: 45,
     fontWeight: '600',
     color: '#FFFFFF',
+    marginBottom: 10,
   },
   inputContainer: {
     backgroundColor: 'white',
@@ -158,7 +174,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '300',
     color: '#757083',
-    opacity: 1,
     marginBottom: 10,
   },
   colorContainer: {
